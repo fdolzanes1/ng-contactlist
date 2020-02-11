6a9196b0b37d2341e2ef203439c23a1663dd3082
fdolzanes1 <fdolzanes1@gmail.com>
2020-02-11 13:22:20 -0400

    Update the contact service

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact';
+import { ContactService } from '../contact.service';
+import { ContactDetailsComponent } from '../contact-details/contact-details.component'
 
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
+  styleUrls: ['./contact-list.component.css'],
+  providers: [ContactService]
 })
 export class ContactListComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['name', 'email'];
+  data: Contact[] = [];
+  isLoadingResults = true;
+
+  constructor(private contactService: ContactService) { }
 
   ngOnInit() {
+    this.contactService.getContacts()
+    .subscribe((res: any) => {
+      this.data = res;
+      console.log(this.data);
+      this.isLoadingResults = false;
+    }, err => {
+      console.log(err);
+      this.isLoadingResults = false;
+    });
   }
+ 
 
 }
